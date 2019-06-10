6e60144d59b0f54693296e5be97fe5abc39ccb80
V02002 <you@example.com>
2019-06-10 16:31:48 +0800

    xiaomi

---
+++ mobileXiaomi/dist/js/chunk-3d8d8dd4.b996481d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-3d8d8dd4"],{5200:function(n,c,e){e("a29f"),e("9202")},9202:function(n,c,e){},a1f4:function(n,c,e){"use strict";e.r(c);var u=function(){var n=this,c=n.$createElement,e=n._self._c||c;return e("div")},t=[],f=e("2b0e"),a=e("b970");e("c4cf"),e("5200");f["a"].use(a["p"]).use(a["r"]);var i=void 0,o=e("2877"),d=Object(o["a"])(i,u,t,!1,null,null,null);c["default"]=d.exports},b9ed:function(n,c,e){},c4cf:function(n,c,e){e("a29f"),e("b9ed")}}]);
+//# sourceMappingURL=chunk-3d8d8dd4.b996481d.js.map
---
+++ xiaomi/dist/js/checkout~confirm.7f78e64f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["checkout~confirm"],[]]);
+//# sourceMappingURL=checkout~confirm.7f78e64f.js.map
